01ccae2d058a8565444f79a89bbabaa7afda8b61
Samagra Agrawal <samagra110@gmail.com>
2024-02-11 23:25:26 +0530

    design NewsItem component

--- src/components/News.js
+++ src/components/News.js
@@ -4,11 +4,18 @@ import NewsItem from './NewsItem'
 export class News extends Component {
     render() {
         return (
-            <div>
-                This is news component
-                <NewsItem></NewsItem>
-                <NewsItem></NewsItem>
-                <NewsItem></NewsItem>
+            <div className='container my-3'>
+                <h1>Top Headlines</h1>
+                <div className="row my-3">
+                    <div className="col-md-4"><NewsItem title="News1 Title" description="News1 Description" imageUrl="https://dims.apnews.com/dims4/default/c8cfc13/2147483647/strip/true/crop/8383x4715+0+437/resize/1440x810!/quality/90/?url=https%3A%2F%2Fassets.apnews.com%2F1a%2Fc7%2F30566bcd42da83dbda4afa0ddbee%2F2cd3285d184243029e1d5d0abe4a3a27"/></div>
+                    <div className="col-md-4"><NewsItem title="News2 Title" description="News2 Description" imageUrl="https://dims.apnews.com/dims4/default/c8cfc13/2147483647/strip/true/crop/8383x4715+0+437/resize/1440x810!/quality/90/?url=https%3A%2F%2Fassets.apnews.com%2F1a%2Fc7%2F30566bcd42da83dbda4afa0ddbee%2F2cd3285d184243029e1d5d0abe4a3a27"/></div>
+                    <div className="col-md-4"><NewsItem title="News3 Title" description="News3 Description" imageUrl="https://dims.apnews.com/dims4/default/c8cfc13/2147483647/strip/true/crop/8383x4715+0+437/resize/1440x810!/quality/90/?url=https%3A%2F%2Fassets.apnews.com%2F1a%2Fc7%2F30566bcd42da83dbda4afa0ddbee%2F2cd3285d184243029e1d5d0abe4a3a27"/></div>
+                </div>
+                <div className="row my-3">
+                    <div className="col-md-4"><NewsItem title="News4 Title" description="News4 Description" imageUrl="https://dims.apnews.com/dims4/default/c8cfc13/2147483647/strip/true/crop/8383x4715+0+437/resize/1440x810!/quality/90/?url=https%3A%2F%2Fassets.apnews.com%2F1a%2Fc7%2F30566bcd42da83dbda4afa0ddbee%2F2cd3285d184243029e1d5d0abe4a3a27"/></div>
+                    <div className="col-md-4"><NewsItem title="News5 Title" description="News5 Description" imageUrl="https://dims.apnews.com/dims4/default/c8cfc13/2147483647/strip/true/crop/8383x4715+0+437/resize/1440x810!/quality/90/?url=https%3A%2F%2Fassets.apnews.com%2F1a%2Fc7%2F30566bcd42da83dbda4afa0ddbee%2F2cd3285d184243029e1d5d0abe4a3a27"/></div>
+                    <div className="col-md-4"><NewsItem title="News6 Title" description="News6 Description" imageUrl="https://dims.apnews.com/dims4/default/c8cfc13/2147483647/strip/true/crop/8383x4715+0+437/resize/1440x810!/quality/90/?url=https%3A%2F%2Fassets.apnews.com%2F1a%2Fc7%2F30566bcd42da83dbda4afa0ddbee%2F2cd3285d184243029e1d5d0abe4a3a27"/></div>
+                </div>
             </div>
         )
     }
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -2,10 +2,18 @@ import React, { Component } from 'react'
 
 export class NewsItem extends Component {
   render() {
+    let { title, description, imageUrl } = this.props
     return (
-        <div>
-            This is news Item
+      <div className='container'>
+        <div className="card" style={{ width: "18rem" }}>
+          <img src= {imageUrl} className="card-img-top" alt="..." />
+          <div className="card-body">
+            <h5 className="card-title">{title}</h5>
+            <p className="card-text">{description}</p>
+            <a href="/newsDetail" className="btn btn-primary">Read More</a>
+          </div>
         </div>
+      </div>
     )
   }
 }
