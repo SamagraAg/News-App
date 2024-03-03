443f2b68c9f056cbde5019e957fb37536f960630
Samagra Agrawal <samagra110@gmail.com>
2024-03-03 19:48:09 +0530

    Fetch news from endPoint and set default image

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react'
-
+const defaultUrl = `https://st.depositphotos.com/1011646/1255/i/450/depositphotos_12553000-stock-photo-breaking-news-screen.jpg`
 export class NewsItem extends Component {
   render() {
     let { title, description, imageUrl, url } = this.props
     return (
       <div className='my-2'>
         <div className="card" style={{ width: "18rem" }}>
-          <img src={imageUrl} style={{ width: "18rem", height: "10rem"}} className="card-img-top" alt="..." />
+          <img src={imageUrl!=null ? imageUrl : defaultUrl} style={{ width: "18rem", height: "10rem"}} className="card-img-top" alt="..." />
           <div className="card-body">
             <h5 className="card-title">{title}</h5>
             <p className="card-text">{description}</p>
